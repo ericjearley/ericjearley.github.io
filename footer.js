d6a92e4dd935b86f7d0c2f7725a6449b25885adc
ericearley <earley.eric@gmail.com>
2016-10-18 23:42:52 -0500

    20161018
    Added table class footer
    Updated footer to make external links and address centered even when in
    mobile mode
    Added recentpublications.js to make all recent publications uniform
    across all pages
    Added photos for index page
    Added full publication titles to publication pages
    Added link button to full text to publication pages

--- footer.js
+++ footer.js
@@ -29,30 +29,40 @@ document.getElementById('footer-wrapper').innerHTML =
 '				<h2>Get in touch</h2>' +
 '				<div class="row">' +
 '					<div class="4u 12u(mobile)">' +
-'						<dl class="contact">' +
-'							<dt> <i id="twittericon" class="icon fa-twitter-square"></i></dt>' +
-'							<dd><a href="https://twitter.com/EricJEarley">Twitter</a></dd>' +
-'							<dt> <i class="icon fa-linkedin-square"></i></dt>' +
-'							<dd><a href="https://www.linkedin.com/in/ericearley">LinkedIn</a></dd>' +
-'							<dt> <i class="icon fa-youtube-square"></i></dt>' +
-'							<dd><a href="https://www.youtube.com/channel/UCiwT2dGNVAU-8R7c-UAEVVw">YouTube</a></dd>' +
-'							<dt> <i class="icon fa-envelope"></i></dt>' +
-'							<dd><a href="mailto:earley.eric@gmail.com">Email</a></dd>' +
-'						</dl>' +
+'						<table class="footer" style="width: 100px">' +
+'							<tr>' +
+'								<td><i id="twittericon" class="icon fa-twitter-square"></i></td>' +
+'								<td><a href="https://twitter.com/EricJEarley">Twitter</a></td>' +
+'							</tr>' +
+'							<tr>' +
+'								<td><i class="icon fa-linkedin-square"></i></td>' +
+'								<td><a href="https://www.linkedin.com/in/ericearley">LinkedIn</a></td>' +
+'							</tr>' +
+'							<tr>' +
+'								<td><i class="icon fa-youtube-square"></i></td>' +
+'								<td><a href="https://www.youtube.com/channel/UCiwT2dGNVAU-8R7c-UAEVVw">YouTube</a></td>' +
+
+'							</tr>' +
+'							<tr>' +
+'								<td><i class="icon fa-envelope"></i></td>' +
+'								<td><a href="mailto:earley.eric@gmail.com">Email</a></td>' +
+'							</tr>' +
+'						</table>' +
 '					</div>' +
 //Address
 '					<div class="8u 12u(mobile)">' +
-'						<dl class="contact">' +
-'							<dt>Address</dt>' +
-'							<dd>' +
-'							Rehabilitation Institute of Chicago<br />' +
-'							345 E. Superior St., Rm. 1431<br />' +
-'							Chicago, IL 60611<br />' +
-'							USA' +
-'							</dd>' +
-'							<dt>Phone</dt>' +
-'							<dd>(312) 238-2079</dd>' +
-'						</dl>' +
+'						<table class="footer" style="width: 350px">' +
+'							<tr>' +
+'								<td><b>Address</b></td>' +
+'								<td>Rehabilitation Institute of Chicago<br />' +
+'								345 E. Superior St., Rm. 1431<br />' +
+'								Chicago, IL 60611<br />' +
+'								USA</td>' +
+'							</tr>' +
+'							<tr>' +
+'								<td><b>Phone<b></td>' +
+'								<td>(312) 238-2079</td>' +
+'						</table>' +
 '					</div>' +
 '				</div>' +
 '			</section>' +
---
+++ recentpublications.js
@@ -0,0 +1,62 @@
+document.getElementById('recentpublications').innerHTML =
+'	<div class="inner">' +
+'		<div class="container">' +
+'			<div class="row">' +
+'				<div class="8u 12u(mobile)">' +
+//Article list
+'					<section class="box article-list">' +
+'						<h2 class="icon fa-file-text-o">Recent Publications</h2>' +
+//Excerpt
+'						<article class="box excerpt">' +
+'							<a href="http://www.ericjearley.com/publications/BioRob2016.html" class="image left"><img src="http://www.ericjearley.com/images/pic04.jpg" alt="" /></a>' +
+'							<div>' +
+'								<header>' +
+'									<span class="date">June 2016</span>' +
+'									<h3><a href="http://www.ericjearley.com/publications/BioRob2016.html">BioRob 2016</a></h3>' +
+'								</header>' +
+'							</div>' +
+'						</article>' +
+//Excerpt
+'						<article class="box excerpt">' +
+'							<a href="http://www.ericjearley.com/publications/Frontiers2016.html" class="image left"><img src="http://www.ericjearley.com/images/pic05.jpg" alt="" /></a>' +
+'							<div>' +
+'								<header>' +
+'									<span class="date">February 2016</span>' +
+'									<h3><a href="http://www.ericjearley.com/publications/Frontiers2016.html">Frontiers in Neuroscience 2016</a></h3>' +
+'								</header>' +
+'							</div>' +
+'						</article>' +
+//Excerpt
+'						<article class="box excerpt">' +
+'							<a href="http://www.ericjearley.com/publications/EMBC2014.html" class="image left"><img src="http://www.ericjearley.com/images/pic06.jpg" alt="" /></a>' +
+'							<div>' +
+'								<header>' +
+'									<span class="date">September 2014</span>' +
+'									<h3><a href="http://www.ericjearley.com/publications/EMBC2014.html">EMBC 2014</a></h3>' +
+'								</header>' +
+'							</div>' +
+'						</article>' +
+'					</section>' +
+'				</div>' +
+'				<div class="4u 12u(mobile)">' +
+//Spotlight
+'					<section class="box spotlight">' +
+'						<h2 class="icon fa-file-text-o">Spotlight</h2>' +
+'						<article>' +
+'							<a href="http://www.ericjearley.com/publications/Frontiers2016.html" class="image featured"><img src="http://www.ericjearley.com/images/pic07.jpg" alt=""></a>' +
+'							<header>' +
+'								<h3><a href="http://www.ericjearley.com/publications/Frontiers2016.html">Dual Window Pattern Recognition Classifier for Improved Partial-Hand Prosthesis Control</a></h3>' +
+'								<p>Frontiers in Neuroscience, 2016</p>' +
+'							</header>' +
+'							<p>Submitted as part of the research topic: <b>Current challenges and new avenues in neural interfacing: from nanomaterials' +
+'							and microfabrication state-of-the-art, to advanced control-theoretical and signal-processing principles</b></p>' +
+'							<footer>' +
+'								<a href="http://www.ericjearley.com/publications/Frontiers2016.html" class="button alt icon fa-file-o">Continue Reading</a>' +
+'							</footer>' +
+'						</article>' +
+'					</section>' +
+'				</div>' +
+'			</div>' +
+'		</div>' +
+'	</div>' +
+'</div>';
