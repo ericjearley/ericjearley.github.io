c0baf3b878ef60e126fdec69b46d62d8b4668b9e
ericearley <earley.eric@gmail.com>
2016-11-23 14:09:54 -0600

    20161123_1
    Modified bg images to tile properly
    Added images for publications
    Added header image for outreach
    Added MATLAB and SolidWorks workshops

--- recentpublications.js
+++ recentpublications.js
@@ -8,7 +8,7 @@ document.getElementById('recentpublications').innerHTML =
 '						<h2 class="icon fa-file-text-o">Recent Publications</h2>' +
 //Excerpt
 '						<article class="box excerpt">' +
-'							<a href="http://www.ericjearley.com/publications/BioRob2016.html" class="image left"><img src="http://www.ericjearley.com/images/pic04.jpg" alt="" /></a>' +
+'							<a href="http://www.ericjearley.com/publications/BioRob2016.html" class="image left"><img src="http://www.ericjearley.com/images/RAS.png" alt="" /></a>' +
 '							<div>' +
 '								<header>' +
 '									<span class="date">June 2016</span>' +
@@ -18,7 +18,7 @@ document.getElementById('recentpublications').innerHTML =
 '						</article>' +
 //Excerpt
 '						<article class="box excerpt">' +
-'							<a href="http://www.ericjearley.com/publications/Frontiers2016.html" class="image left"><img src="http://www.ericjearley.com/images/pic05.jpg" alt="" /></a>' +
+'							<a href="http://www.ericjearley.com/publications/Frontiers2016.html" class="image left"><img src="http://www.ericjearley.com/images/frontiers.jpg" alt="" /></a>' +
 '							<div>' +
 '								<header>' +
 '									<span class="date">February 2016</span>' +
@@ -28,7 +28,7 @@ document.getElementById('recentpublications').innerHTML =
 '						</article>' +
 //Excerpt
 '						<article class="box excerpt">' +
-'							<a href="http://www.ericjearley.com/publications/EMBC2014.html" class="image left"><img src="http://www.ericjearley.com/images/pic06.jpg" alt="" /></a>' +
+'							<a href="http://www.ericjearley.com/publications/EMBC2014.html" class="image left"><img src="http://www.ericjearley.com/images/EMBS.png" alt="" /></a>' +
 '							<div>' +
 '								<header>' +
 '									<span class="date">September 2014</span>' +
@@ -43,7 +43,7 @@ document.getElementById('recentpublications').innerHTML =
 '					<section class="box spotlight">' +
 '						<h2 class="icon fa-file-text-o">Spotlight</h2>' +
 '						<article>' +
-'							<a href="http://www.ericjearley.com/publications/Frontiers2016.html" class="image featured"><img src="http://www.ericjearley.com/images/pic07.jpg" alt=""></a>' +
+'							<a href="http://www.ericjearley.com/publications/Frontiers2016.html" class="image featured"><img src="http://www.ericjearley.com/images/frontiers.jpg" alt=""></a>' +
 '							<header>' +
 '								<h3><a href="http://www.ericjearley.com/publications/Frontiers2016.html">Dual Window Pattern Recognition Classifier for Improved Partial-Hand Prosthesis Control</a></h3>' +
 '								<p>Frontiers in Neuroscience, 2016</p>' +
