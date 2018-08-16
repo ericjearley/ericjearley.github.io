77e5442ca6a3d8bc2d6953b8e1fa2018a7486bfc
ericjearley <earley.eric@gmail.com>
2018-08-15 19:39:26 -0500

    20180815_01
    Added interactive demos
    Streamlined footer
    Centered images on index
    Updated header and footer on all pages

--- assets/js/kinematics.js
+++ assets/js/kinematics.js
@@ -2,6 +2,7 @@
 
 function init(){
 	window.addEventListener("keydown",doKeyDown,false);
+	window.addEventListener("keyup",doKeyUp,false);
 	timer=setInterval(draw,10);
 	return timer;
 }
@@ -15,6 +16,11 @@ var pi = Math.PI;
 
 var T1 = pi/6;
 var T2 = T1 + pi/6
+var O1 = 0;
+var O2 = 0;
+var Vx = 0;
+var Vy = 0;
+
 var L1 = 100;
 var L2 = 100;
 var R = 10;
@@ -30,81 +36,65 @@ ctx.lineWidth = '10';
 ctx.translate(250,250);
 
 function doKeyDown(e){
-	/*forward kinematics*/
 	if(e.keyCode==65){//A
-	T1 = T1+pi/30;
+	O1 = pi/60;
 	}
 	else if(e.keyCode==68){//D
-	T1 = T1-pi/30;
+	O1 = -pi/60;
 	}
 	if(e.keyCode==87){//W
-	T2 = T2-pi/30;
+	O2 = -pi/60;
 	}
 	else if(e.keyCode==83){//W
-	T2 = T2+pi/30;
-	}
-
-	/*inverse kinematics*/
-	var C2 = new Circle(L1*Math.cos(T1)+L2*Math.cos(T1+T2),L1*Math.sin(T1)+L2*Math.sin(T1+T2),R);
-	if(e.keyCode==74){//J
-		C2.x = C2.x - 8;
-		if ((Math.pow(C2.x,2)+Math.pow(C2.y,2))>Math.pow(((L1+L2)),2)){
-			C2.x = C2.x+8;
-		}
-		else{
-			T2 = Math.acos((Math.pow(C2.x,2)+Math.pow(C2.y,2)-Math.pow(L1,2)-Math.pow(L2,2))/(2*L1*L2))*Math.sign(T2);
-			T1 = -(Math.atan2(C2.y,C2.x)+Math.atan2((L2*Math.sin(T2)),(L1+L2*Math.cos(T2))));
-		}
-	}
-	else if(e.keyCode==76){//L
-		C2.x = C2.x + 8;
-		if ((Math.pow(C2.x,2)+Math.pow(C2.y,2))>Math.pow(((L1+L2)),2)){
-			C2.x = C2.x-8;
-		}
-		else{
-			T2 = Math.acos((Math.pow(C2.x,2)+Math.pow(C2.y,2)-Math.pow(L1,2)-Math.pow(L2,2))/(2*L1*L2))*Math.sign(T2);
-			T1 = -(Math.atan2(C2.y,C2.x)+Math.atan2((L2*Math.sin(T2)),(L1+L2*Math.cos(T2))));
-		}
-	}
-
-	if(e.keyCode==73){//I
-		C2.y = C2.y - 8;
-		if ((Math.pow(C2.x,2)+Math.pow(C2.y,2))>Math.pow(((L1+L2)),2)){
-			C2.y = C2.y+8;
-		}
-		else{
-			T2 = Math.acos((Math.pow(C2.x,2)+Math.pow(C2.y,2)-Math.pow(L1,2)-Math.pow(L2,2))/(2*L1*L2))*Math.sign(T2);
-			T1 = -(Math.atan2(C2.y,C2.x)+Math.atan2((L2*Math.sin(T2)),(L1+L2*Math.cos(T2))));
-		}
-	}
-	else if(e.keyCode==75){//K
-		C2.y = C2.y + 8;
-		if ((Math.pow(C2.x,2)+Math.pow(C2.y,2))>Math.pow(((L1+L2)),2)){
-			C2.y = C2.y-8;
-		}
-		else{
-			T2 = Math.acos((Math.pow(C2.x,2)+Math.pow(C2.y,2)-Math.pow(L1,2)-Math.pow(L2,2))/(2*L1*L2))*Math.sign(T2);
-			T1 = -(Math.atan2(C2.y,C2.x)+Math.atan2((L2*Math.sin(T2)),(L1+L2*Math.cos(T2))));
-		}
-	}
-
-	/*bound angles*/
-	if (T1>pi){
-		T1 = T1-2*pi;
-	} else if(T1<-pi){
-		T1 = T1+2*pi;
+	O2 = pi/60;
 	}
 	
-	if (T2>pi){
-		T2 = T2-2*pi;
-	} else if(T2<-pi){
-		T2 = T2+2*pi;
+	if(e.keyCode==74){//J
+	Vx = 4;
+	}
+	else if(e.keyCode==76){//L
+	Vx = -4;
+	}
+	if(e.keyCode==73){//I
+	Vy = 4;
+	}
+	else if(e.keyCode==75){//K
+	Vy = -4;
+	}
+}
+
+function doKeyUp(e){
+	if(e.keyCode==65){//A
+	O1 = 0;
+	}
+	else if(e.keyCode==68){//D
+	O1 = 0;
+	}
+	if(e.keyCode==87){//W
+	O2 = 0;
+	}
+	else if(e.keyCode==83){//W
+	O2 = 0;
+	}
+	
+	if(e.keyCode==74){//J
+	Vx = 0;
+	}
+	else if(e.keyCode==76){//L
+	Vx = 0;
+	}
+	if(e.keyCode==73){//I
+	Vy = 0;
+	}
+	else if(e.keyCode==75){//K
+	Vy = 0;
 	}
 }
 
 function draw(){
 	/*T1 = T1+pi/360;
 	T2 = T2-pi/180;*/
+	updatespeed();
 	ctx.clearRect(-250,-250,canvas.width,canvas.height);
 	
 	var C1 = new Circle(L1*Math.cos(T1),L1*Math.sin(T1),R);
@@ -131,6 +121,45 @@ function draw(){
 	ctx.moveTo(C1.x,C1.y);
 	ctx.lineTo(C2.x,C2.y);
 	ctx.stroke();
+}
+
+function updatespeed(){
+	/*forward kinematics*/
+	T1 = T1+O1;
+	T2 = T2+O2;
+
+	/*inverse kinematics*/
+	var C2 = new Circle(L1*Math.cos(T1)+L2*Math.cos(T1+T2),L1*Math.sin(T1)+L2*Math.sin(T1+T2),R);
+	C2.x = C2.x - Vx;
+	if ((Math.pow(C2.x,2)+Math.pow(C2.y,2))>Math.pow(((L1+L2)),2)){
+		C2.x = C2.x+Vx;
+	}
+	else{
+		T2 = Math.acos((Math.pow(C2.x,2)+Math.pow(C2.y,2)-Math.pow(L1,2)-Math.pow(L2,2))/(2*L1*L2))*Math.sign(T2);
+		T1 = -(Math.atan2(C2.y,C2.x)+Math.atan2((L2*Math.sin(T2)),(L1+L2*Math.cos(T2))));
+	}
+
+	C2.y = C2.y - Vy;
+	if ((Math.pow(C2.x,2)+Math.pow(C2.y,2))>Math.pow(((L1+L2)),2)){
+		C2.y = C2.y+Vy;
+	}
+	else{
+		T2 = Math.acos((Math.pow(C2.x,2)+Math.pow(C2.y,2)-Math.pow(L1,2)-Math.pow(L2,2))/(2*L1*L2))*Math.sign(T2);
+		T1 = -(Math.atan2(C2.y,C2.x)+Math.atan2((L2*Math.sin(T2)),(L1+L2*Math.cos(T2))));
+	}
+
+	/*bound angles*/
+	if (T1>pi){
+		T1 = T1-2*pi;
+	} else if(T1<-pi){
+		T1 = T1+2*pi;
+	}
+	
+	if (T2>pi){
+		T2 = T2-2*pi;
+	} else if(T2<-pi){
+		T2 = T2+2*pi;
+	}
 }
 
 function Circle(x,y,r){
